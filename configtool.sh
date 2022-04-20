bffa63986c90738e04fab255c66678fe95eaf06b
Rodrigo <r.martin.nunez@student.tue.nl>
2022-04-20 16:15:39 +0200

    Fixed script

--- configtool.sh
+++ configtool.sh
@@ -10,114 +10,111 @@ alacrittyPATH=~/.config/alacritty/alacritty.yml
 tmuxPATH=~/.tmux.conf
 dunstPATH=~/.config/dunst/dunstrc
 
-setI3() {
-	mv $i3PATH ${i3PATH}.backup
-	cp ${PWD}/i3/config $i3PATH
+## Functions: 
+# arg1 = 1: set
+# arg1 = 0: fetch
+I3() {
+	if [ "$1" -eq "1" ]; then
+		mv $i3PATH ${i3PATH}.backup
+		cp ${PWD}/i3/config $i3PATH
+	
+	else
+		cp $i3PATH ${PWD}/i3/config
+	fi
 }
 
-setALACRITTY() {
-	mv $alacrittyPATH ${alacrittyPATH}.backup
-	cp ${PWD}/alacritty/alacritty.yml $alacrittyPATH
+TMUX() {
+	if [ "$1" -eq "1" ]; then
+		mv $tmuxPATH ${tmuxPATH}.backup
+		cp ${PWD}/tmux/.tmux.conf $tmuxPATH
+	
+	else
+		cp $tmuxPATH ${PWD}/tmux/.tmux.conf
+	fi
 }
 
-setTMUX() {
-	mv $tmuxPATH ${tmuxPATH}.backup
-	cp ${PWD}/tmux/.tmux.conf $tmuxPATH
+ALACRITTY() {
+	if [ "$1" -eq "1" ]; then
+		mv $alacrittyPATH ${alacrittyPATH}.backup
+		cp ${PWD}/alacritty/alacritty.yml $alacrittyPATH
+	
+	else
+		cp $alacrittyPATH ${PWD}/alacritty/alacritty.yml
+	fi
 }
 
-setDUNST() {
-	mv $dunstPATH ${dunstPATH}.backup
-	cp ${PWD}/tmux/.tmux.conf $dunstPATH
+DUNST() {
+	if [ "$1" -eq "1" ]; then
+		mv $dunstPATH ${dunstPATH}.backup
+		cp ${PWD}/dunst/dunstrc $dunstPATH
+	
+	else
+		cp $dunstPATH ${PWD}/dunst/dunstrc
+	fi
 }
 
-fetchI3() {
-	cp $i3PATH ${PWD}/i3/config
-}
-
-fetchALACRITTY() {
-	cp $alacrittyPATH ${PWD}/alacritty/alacritty.yml
-}
-
-fetchTMUX() {
-	cp $tmuxPATH ${PWD}/tmux/.tmux.conf
-}
-
-fetchDUNST() {
-	cp $dunstPATH ${PWD}/dunst/dunstrc
-}
 
 #######################################################
 #######################################################
 # 	Read arguments				      #
 #######################################################
 #######################################################
-while test $# -gt 0; do
-	case "$1" in
-    		-h|--help)
-			echo "Run the script as follows: ./script [action] [argument1 argument2 ...] [action2] [argument1 argument2 ...]"
-			echo
-			echo "Possible actions are:"
-			echo "-s | --set) Sets configs from repo to system"
-			echo "-f | --fetch) Copies configs from system to repo"
-			echo
-			echo "Possible arguments are:"
-			echo "i3"
-			echo "alacritty"
-			echo "tmux"
-			echo "dunst"
-			exit 0
-			;;
+while getopts hs:f: flag
+do
+    case "${flag}" in
+    	-h)
+				echo "Run the script as follows: ./script action [program]"
+				echo
+				echo "Possible actions are:"
+				echo "-s Sets configs from repo to system"
+				echo "-f Copies configs from system to repo"
+				echo "-h Prints the help section"
+				echo
+				echo "Possible program are:"
+				echo "i3"
+				echo "alacritty"
+				echo "tmux"
+				echo "dunst"
+				exit 0
+				;;
 
-		-s | --set)
-			shift
-			for i in $@
-			do 	
-  			if [ "$i" == "i3" ]; then
-					setI3
-	
-				elif [ "$i" == "tmux" ]; then
-					setTMUX
-
-				elif [ "$i" == "alacritty" ]; then
-					setALACRITTY
-		
-				elif [ "$i" == "dunst" ]; then
-					setDUNST	
-
-				else
-					echo -e "Invalid option: $i\n"
-		
-				fi
-			done
-			shift
-			;;
-
-		-f | --fetch)
-			shift
-			for i in $@
-			do 	
-  			if [ "$i" == "i3" ]; then
-				fetchI3
+		-s)
+  		if [ "$i" == "i3" ]; then
+				I3 1
 	
 			elif [ "$i" == "tmux" ]; then
-				fetchTMUX
+				TMUX 1
 
 			elif [ "$i" == "alacritty" ]; then
-				fetchALACRITTY
+				ALACRITTY 1
 		
 			elif [ "$i" == "dunst" ]; then
-				fetchDUNST	
+				DUNST	1
 
 			else
 				echo -e "Invalid option: $i\n"
-		
+				exit 0
 			fi
-			done
-			shift
 			;;
-		*)
-			echo "Please specify option!"
-			break
+
+		-f)
+  		if [ "$i" == "i3" ]; then
+				I3 0
+	
+			elif [ "$i" == "tmux" ]; then
+				TMUX 0
+
+			elif [ "$i" == "alacritty" ]; then
+				ALACRITTY 0
+		
+			elif [ "$i" == "dunst" ]; then
+				DUNST	0
+
+			else
+				echo -e "Invalid option: $i\n"
+				exit 0
+			fi
 			;;
-	esac
+    esac
 done
+
