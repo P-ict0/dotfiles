ede6d2040733f566b7bbcb2442c8e306e2ce620f
Rodrigo <r.martin.nunez@student.tue.nl>
2023-10-28 11:00:19 +0200

    LazyNvim config

---
+++ clean-arch/config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "c",
+      "cpp",
+      "lua",
+      "python",
+      "vimdoc",
+      "json",
+      "bash",
+      "markdown",
+      "regex",
+      "yaml",
+    },
+  },
+}
