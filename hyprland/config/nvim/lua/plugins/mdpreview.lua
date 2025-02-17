b5ea92b9fb085206c2306316a83759c792eb63e2
P-ict0 <rodrigomartinnunez@gmail.com>
2025-02-17 16:14:14 +0100

    MD preview

--- hyprland/config/nvim/lua/plugins/mdpreview.lua
+++ hyprland/config/nvim/lua/plugins/mdpreview.lua
@@ -1,20 +1,29 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-      vim.g.mkdp_theme = "dark"
-    end,
-    ft = { "markdown" },
-    keys = {
-      {
-        "<leader>cp",
-        ft = "markdown",
-        "<cmd>MarkdownPreviewToggle<cr>",
-        desc = "Markdown Preview",
-      },
-    },
-  },
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+			vim.g.mkdp_theme = "dark"
+		end,
+		ft = { "markdown" },
+		keys = {
+			{
+				"<leader>cp",
+				ft = "markdown",
+				"<cmd>MarkdownPreviewToggle<cr>",
+				desc = "Markdown Preview",
+			},
+		},
+		config = function()
+			vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+			vim.cmd([[
+      function! OpenMarkdownPreview(url)
+        execute "silent ! firefox --new-window " . a:url
+      endfunction
+      do FileType
+    ]])
+		end,
+	},
 }
