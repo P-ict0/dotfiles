19a7c5b907b678cd590b11ec4204c6bd06b0bfee
P-ict0 <rodrigomartinnunez@gmail.com>
2025-02-18 14:00:16 +0100

    More fish stuff

--- hyprland/config/fish/config.fish
+++ hyprland/config/fish/config.fish
@@ -1,12 +1,13 @@
 set -g fish_greeting
 
 if status is-interactive
+    # Commands to run when starting an interactive session
     starship init fish | source
 end
 
 # List Directory
-alias l='eza -lh  --icons=auto' # long list
-alias ls='eza -1   --icons=auto' # short list
+alias ls='eza -1 --icons=auto' # short list
+alias l='eza -lh   --icons=auto' # long list
 alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
 alias ld='eza -lhD --icons=auto' # long list dirs
 alias lt='eza --icons=auto --tree' # list folder as tree
